376a450e3feca6387bdae10babb84408559dbe53
TemitopeAyokuHO <138882912+TemitopeAyokuHO@users.noreply.github.com>
2024-09-20 13:42:25 +0100

    Application setup (#2)

---
+++ apps/rod/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const SummaryPageBehaviour = require('hof').components.summary;
+
+module.exports = {
+  name: 'rod',
+  steps: {
+    '/start': {
+      fields: ['name'],
+      next: '/confirm'
+    },
+    
+    '/confirm': {
+      behaviours: [SummaryPageBehaviour],
+      sections: require('./sections/summary-data-sections'),
+      next: '/confirmation'
+    },
+    '/confirmation': {
+      clearSession: true
+    }
+  }
+};
