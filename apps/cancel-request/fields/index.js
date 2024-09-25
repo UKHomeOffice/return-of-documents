e119f1b63917bb2521bf2edf43ffb03881d31dbc
Mike Maratheftis <160880141+MikeMaratheftis@users.noreply.github.com>
2024-09-25 12:43:09 +0100

    ROD 54 - Skeleton for Cancel Request (#5)
    * Merge with master
    
    * fixed linting errors
    
    * added pathing and correct naming convention for cancel request flow
    
    ---------
    
    Co-authored-by: Mike Maratheftis <mike.maratheftis@digital.homeoffice.gov.uk>
    Co-authored-by: TemitopeAyokuHO <138882912+TemitopeAyokuHO@users.noreply.github.com>

--- apps/cancel-request/fields/index.js
+++ apps/cancel-request/fields/index.js
@@ -1,7 +1,118 @@
 'use strict';
+const dateComponent = require('hof').components.date;
+const countries = require('hof').utils.countries();
 
 module.exports = {
   name: {
     mixin: 'input-text'
+  },
+  'cnc-main-applicant-full-name': {
+    mixin: 'input-text',
+    validate: ['required', { type: 'maxlength', arguments: 250 }, { type: 'minlength', arguments: 3 }],
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-main-applicant-dob': dateComponent('cnc-main-applicant-dob', {
+    mixin: 'input-date',
+    legend: {
+      className: 'bold'
+    },
+    validate: ['required']
+  }),
+  'cnc-main-applicant-nationality': {
+    mixin: 'select',
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    },
+    options: [{
+      value: '',
+      label: 'fields.cnc-main-applicant-nationality.options.none_selected'
+    }].concat(countries),
+    labelClassName: 'bold'
+  },
+  'cnc-who-is-completing': {
+    mixin: 'radio-group',
+    options: ['applicant', 'legal-rep', 'sponsor', 'guardian'],
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-who-is-representing': {
+    mixin: 'radio-group',
+    options: ['applicant', 'document-holder'],
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-sponsor-type': {
+    mixin: 'radio-group',
+    options: ['british-sponsor', 'settle-sponsor', 'eea-sponsor'],
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-dependant-or-guardian': {
+    mixin: 'radio-group',
+    options: ['british-sponsor', 'settle-sponsor', 'eea-sponsor'],
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-reason-for-application': {
+    mixin: 'radio-group',
+    options: ['visa', 'british-citizenship', 'leave-to-remain',
+      'ntl-or-brp', 'european-settlement-scheme', 'settlement', 'toc-or-brp'],
+
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-visa-type': {
+    mixin: 'radio-group',
+    options: ['british-overseas-national', 'exceptional-talent',
+      'skilled-worker', 'study', 'temporary-work', 'turkish-national', 'different-type'],
+
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-further-leave': {
+    mixin: 'radio-group',
+    options: ['fp', 'm', 'ir', 'hro'],
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-reference-number': {
+    mixin: 'radio-group',
+    options: ['record-number', 'case-id', 'ho-ref-number', 'payment-ref-number', 'courier-ref-number'],
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-contact-email': {
+    mixin: 'input-text',
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
+  },
+  'cnc-contact-telephone-number': {
+    mixin: 'input-text',
+    validate: 'required',
+    legend: {
+      className: 'bold'
+    }
   }
+
 };
--- apps/cancel-request/index.js
+++ apps/cancel-request/index.js
@@ -6,9 +6,80 @@ module.exports = {
   name: 'cancel-request',
   baseUrl: '/',
   steps: {
-    '/cancel-request-start': {
+    '/cancel-request': {
       fields: ['name'],
-      next: '/confirm'
+      next: '/cancel-request-main-applicant'
+    },
+    '/cancel-request-main-applicant': {
+      fields: ['cnc-main-applicant-full-name', 'cnc-main-applicant-dob', 'cnc-main-applicant-nationality'],
+      next: '/cancel-request-who-completed-form'
+    },
+    '/cancel-request-who-completed-form': {
+      forks: [
+        {
+          target: '/cancel-request-who-representing',
+          condition: {
+            field: 'cnc-who-is-completing',
+            value: 'cnc-legal-rep'
+          },
+          continueOnEdit: false
+        },
+        {
+          target: '/cancel-request-sponsor-type',
+          continueOnEdit: true,
+          condition: {
+            field: 'cnc-who-is-completing',
+            value: 'cnc-sponsor'
+          }
+        },
+        {
+          target: '/cancel-request-dependant-or-guardian',
+          continueOnEdit: true,
+          condition: {
+            field: 'cnc-who-is-completing',
+            value: 'cnc-guardian'
+          }
+        }
+      ],
+      next: '/cancel-request-application'
+    },
+    '/cancel-request-who-representing': {
+      next: '/cancel-request-application'
+    },
+    '/cancel-request-sponsor-type': {
+      next: '/cancel-request-application'
+    },
+    '/cancel-request-dependant-or-guardian': {
+      next: '/cancel-request-application'
+    },
+    '/cancel-request-application': {
+      forks: [
+        {
+          target: '/cancel-request-visa-type',
+          condition: {
+            field: 'cnc-reason-for-application',
+            value: 'cnc-visa'
+          }
+        },
+        {
+          target: '/cancel-request-further-leave',
+          continueOnEdit: true,
+          condition: {
+            field: 'cnc-reason-for-application',
+            value: 'cnc-leave-to-remain'
+          }
+        }
+      ],
+      next: '/cancel-request-reference-number'
+    },
+    '/cancel-request-reference-number': {
+      next: '/cancel-request-contact-details'
+    },
+    '/cancel-request-contact-details': {
+      next: '/cancel-request-confirm'
+    },
+    '/cancel-request-confirm': {
+      next: '/cancellation-received'
     },
     '/confirm': {
       behaviours: [SummaryPageBehaviour],
