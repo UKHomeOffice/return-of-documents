7597fd070f1a9cde1073874849e6fa65f73c2d85
Mike Maratheftis <mike.maratheftis@digital.homeoffice.gov.uk>
2024-10-02 11:38:02 +0100

    removed legend and added a validation message as per PR comments

--- apps/cancel-request/fields/index.js
+++ apps/cancel-request/fields/index.js
@@ -9,25 +9,18 @@ module.exports = {
   'cnc-main-applicant-full-name': {
     mixin: 'input-text',
     validate: ['required', { type: 'maxlength', arguments: 250 }, { type: 'minlength', arguments: 3 }, 'notUrl'],
-    legend: {
-      className: ['govuk-input', 'govuk-!-width-two-thirds']
-    }
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
   },
   'cnc-main-applicant-dob': dateComponent('cnc-main-applicant-dob', {
     mixin: 'input-date',
-    legend: {
-      className: 'bold'
-    },
+    className: 'bold',
     validate: ['required', 'date',
       { type: 'after', arguments: ['120', 'years'] },
       { type: 'before', arguments: ['0', 'days']}]
   }),
   'cnc-main-applicant-nationality': {
     mixin: 'select',
-    validate: ['required', 'notUrl'],
-    legend: {
-      className: ['typeahead', 'govuk-input govuk-!-width-three-quarters']
-    },
+    validate: 'required',
     options: [{
       value: '',
       label: 'fields.cnc-main-applicant-nationality.options.none_selected'
@@ -38,33 +31,25 @@ module.exports = {
     mixin: 'radio-group',
     options: ['applicant', 'legal-rep', 'sponsor', 'guardian'],
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   },
   'cnc-who-is-representing': {
     mixin: 'radio-group',
     options: ['applicant', 'document-holder'],
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   },
   'cnc-sponsor-type': {
     mixin: 'radio-group',
     options: ['british-sponsor', 'settle-sponsor', 'eea-sponsor'],
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   },
   'cnc-dependant-or-guardian': {
     mixin: 'radio-group',
     options: ['british-sponsor', 'settle-sponsor', 'eea-sponsor'],
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   },
   'cnc-reason-for-application': {
     mixin: 'radio-group',
@@ -72,9 +57,7 @@ module.exports = {
       'ntl-or-brp', 'european-settlement-scheme', 'settlement', 'toc-or-brp'],
 
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   },
   'cnc-visa-type': {
     mixin: 'radio-group',
@@ -82,39 +65,29 @@ module.exports = {
       'skilled-worker', 'study', 'temporary-work', 'turkish-national', 'different-type'],
 
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   },
   'cnc-further-leave': {
     mixin: 'radio-group',
     options: ['fp', 'm', 'ir', 'hro'],
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   },
   'cnc-reference-number': {
     mixin: 'radio-group',
     options: ['record-number', 'case-id', 'ho-ref-number', 'payment-ref-number', 'courier-ref-number'],
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   },
   'cnc-contact-email': {
     mixin: 'input-text',
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   },
   'cnc-contact-telephone-number': {
     mixin: 'input-text',
     validate: 'required',
-    legend: {
-      className: 'bold'
-    }
+    className: 'bold'
   }
 
 };
