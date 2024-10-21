20628b747b24ee38f968b08b2c108e4ee89ef9b4
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2024-10-21 12:30:16 +0100

    change summary data section order

--- apps/report-documents-not-received/sections/summary-data-sections.js
+++ apps/report-documents-not-received/sections/summary-data-sections.js
@@ -19,14 +19,6 @@ module.exports = {
       field: 'dnr-nationality'
     },
     {
-      step: '/documents-not-received-contact-details',
-      field: 'dnr-email'
-    },
-    {
-      step: '/documents-not-received-contact-details',
-      field: 'dnr-telephone'
-    },
-    {
       step: '/documents-not-received-application',
       field: 'dnr-application-type'
     },
@@ -37,6 +29,14 @@ module.exports = {
     {
       step: '/documents-not-received-further-leave',
       field: 'dnr-further-leave-to-remain'
+    },
+    {
+      step: '/documents-not-received-contact-details',
+      field: 'dnr-email'
+    },
+    {
+      step: '/documents-not-received-contact-details',
+      field: 'dnr-telephone'
     }
   ]
 };
