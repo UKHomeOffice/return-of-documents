dfc91a4bda323b99755a2ba993fcb6517b7f2bcc
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2024-11-07 16:43:10 +0000

    ROD-127 notify changes

--- apps/rod/behaviours/send-email-notification.js
+++ apps/rod/behaviours/send-email-notification.js
@@ -1,6 +1,5 @@
 const config = require('../../../config');
 const {
-  getFormattedRecordNumber,
   getLabel,
   getValueOfDefault,
   getYesOrNoStr
@@ -24,53 +23,134 @@ const getLabelForField = (req, field) => {
 
 const getWhoCompletedForm = req => {
   const whoCompleted = req.sessionModel.get('who-is-completing');
-  console.log(whoCompleted);
 
   if (whoCompleted === 'applicant') {
     return getLabelForField(req, 'who-is-completing');
   }
 
-  if (whoCompleted === 'legal-rep-name') {
-    const legalRep = getLabelForField(req, 'legal-rep-name');
+  if (whoCompleted === 'who-is-rep') {
+    const legalRep = getLabelForField(req, 'who-is-representing');
     return legalRep + '’s legal representative';
   }
 
   if (whoCompleted === 'sponsor') {
-    return getLabelForField(req, 'cnc-sponsor-type');
+    return getLabelForField(req, 'sponsor-type');
   }
 
   if (whoCompleted === 'guardian') {
-    return getLabelForField(req, 'cnc-dependant-or-guardian');
+    return getLabelForField(req, 'dependant-or-guardian');
   }
 
   return '';
 };
 
 const getApplicationCategory = req => {
-  const applicationReason = req.sessionModel.get('cnc-reason-for-application');
+  const applicationType = req.sessionModel.get('application-type');
 
-  if (applicationReason === 'visa') {
-    return getLabelForField(req, 'cnc-application-visa-type');
+  if (applicationType === 'visa') {
+    return getLabelForField(req, 'visa-type');
   }
 
-  if (applicationReason === 'leave-to-remain') {
-    return getLabelForField(req, 'cnc-further-leave-to-remain');
+  if (applicationType === 'further-leave') {
+    return getLabelForField(req, 'further-leave-to-remain');
   }
 
-  return getLabelForField(req, 'cnc-reason-for-application');
+  return getLabelForField(req, 'application-type');
 };
 
 const getUserDetails = req => {
   return {
-    person_completing: getWhoCompletedForm(req)
+    person_completing: getWhoCompletedForm(req),
+    application_category: getApplicationCategory(req),
+    application_submitted_date: dateFormatter.format(
+      new Date(getValueOfDefault(req, 'date-of-application'))
+    ),
+    cancel_application: getValueOfDefault(req, 'cancel-application'),
+    applicant_passport: getValueOfDefault(
+      req,
+      'is-requesting-passport-to-travel'
+    ),
+    reference_number: '',
+    has_case_id: '',
+    case_id: '',
+    has_ho_reference_number: '',
+    ho_reference_number: '',
+    has_payment_reference_number: '',
+    payment_reference_number: '',
+    has_courier_reference_number: '',
+    courier_reference_number: '',
+    your_documents: getValueOfDefault(req,'yourDocuments'),
+    document_description: getValueOfDefault(req, 'document-description'),
+    applicant_full_name: getValueOfDefault(req, 'main-applicant-full-name'),
+    applicant_dob: dateFormatter.format(
+      new Date(getValueOfDefault(req, 'main-applicant-dob'))
+    ),
+    applicant_nationality: getValueOfDefault(req, 'main-applicant-nationality'),
+    applicant_address: '',
+    delivery_address: getValueOfDefault(req, 'deliveryAddress'),
+    contact_email: getValueOfDefault(req, 'contact-email'),
+    contact_telephone: getValueOfDefault(req, 'contact-telephone')
   };
 };
 
+function getBusinessEmail(req) {
 
+  const applicationType = req.sessionModel.get('application-type');
+
+  if (applicationType === 'visa') {
+    const selectedVisaType = req.sessionModel.get('visa-type');
+    switch (selectedVisaType) {
+      case 'visa-type-british':
+        return;
+      case 'visa-type-talent':
+        return;
+      case 'visa-type-skilled':
+        return;
+      case 'visa-type-study':
+        return;
+      case 'visa-type-temp':
+        return;
+      case 'visa-type-turkish':
+        return;
+    }
+  }
+  if (applicationType === 'british-citizen') {
+    // return british visa email
+    return ''
+  }
+
+  if (applicationType === 'further-leave') {
+    const flrType = req.sessionModel.get('further-leave-to-remain');
+    switch (flrType) {
+      case 'flr-fp':
+        return;
+      case 'flr-m':
+        return;
+      case 'flr-ir':
+        return;
+      case 'flr-hro':
+        return;
+    }
+  }
+
+  if (applicationType === 'not-time-limit') {
+    return ''
+  }
+  if (applicationType === 'eu-settlement-scheme') {
+    return ''
+  }
+  if (applicationType === 'settlement') {
+    return ''
+  }
+  if (applicationType === 'limited-leave-replacement-brp') {
+    return ''
+  }
+  throw new Error('Could not find the business email address');
+}
 module.exports = class SendEmailConfirmation {
   async sendEmailNotification(req, recipientType) {
     const personalisation = getUserDetails(req);
-
+    console.log(personalisation);
 
     // FIXME: use updated template id and emails
     const templateId =
@@ -81,7 +161,7 @@ module.exports = class SendEmailConfirmation {
     const recipientEmailAddress =
       recipientType === USER
         ? req.sessionModel.get('contact-email')
-        : config.govukNotify.caseworkerEmail;
+        : getBusinessEmail(req);
 
     const userOrBusinessStr = () =>
       recipientType === USER ? 'User' : 'Business';
