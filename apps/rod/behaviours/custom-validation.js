a7c856ef098706d122c5bbcf8a3e8dd58bda75cb
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2024-11-07 15:56:59 +0000

    ROD-92 fix formatting

--- apps/rod/behaviours/custom-validation.js
+++ apps/rod/behaviours/custom-validation.js
@@ -1,39 +1,44 @@
 const validators = require('hof/controller/validation/validators');
 
-module.exports = superclass => class extends superclass {
-  validateField(key, req) {
-    const validationErrorFunc = (type, args) =>
-      new this.ValidationError(key, { type: type, arguments: [args] });
+module.exports = superclass =>
+  class extends superclass {
+    validateField(key, req) {
+      const validationErrorFunc = (type, args) =>
+        new this.ValidationError(key, { type: type, arguments: [args] });
 
-    if (key === 'cancel-application') {
-      const dependent = req.form.options.fields[key].dependent;
-      const dependentValue = req.sessionModel.get(dependent.field);
-      if (dependentValue && dependentValue === dependent.value) {
-        return null;
-      }
-    }
-
-    if (req.form.values['rod-reference-number']?.includes('ho-reference-number') &&
-    key === 'rod-ho-reference-number') {
-      const hoRefNum = req.form.values[key];
-      if (hoRefNum) {
-        const valueWithoutSpace = hoRefNum.replace(/\s+/g, '').trim();
-
-        if (!validators.minlength(valueWithoutSpace, 8)) {
-          return validationErrorFunc('minlength', 8);
-        }
-        if (!validators.maxlength(valueWithoutSpace, 8)) {
-          return validationErrorFunc('maxlength', 8);
-        }
-        if (valueWithoutSpace.match(/\W|_/g)) {
-          return validationErrorFunc('specialCharacter');
-        }
-        if (!valueWithoutSpace.match(/^[A-Z]\d+$/i)) {
-          return validationErrorFunc('hoRefNum');
+      if (key === 'cancel-application') {
+        const dependent = req.form.options.fields[key].dependent;
+        const dependentValue = req.sessionModel.get(dependent.field);
+        if (dependentValue && dependentValue === dependent.value) {
+          return null;
         }
       }
-    }
 
-    return super.validateField(key, req);
-  }
-};
+      if (
+        req.form.values['rod-reference-number']?.includes(
+          'ho-reference-number'
+        ) &&
+        key === 'rod-ho-reference-number'
+      ) {
+        const hoRefNum = req.form.values[key];
+        if (hoRefNum) {
+          const valueWithoutSpace = hoRefNum.replace(/\s+/g, '').trim();
+
+          if (!validators.minlength(valueWithoutSpace, 8)) {
+            return validationErrorFunc('minlength', 8);
+          }
+          if (!validators.maxlength(valueWithoutSpace, 8)) {
+            return validationErrorFunc('maxlength', 8);
+          }
+          if (valueWithoutSpace.match(/\W|_/g)) {
+            return validationErrorFunc('specialCharacter');
+          }
+          if (!valueWithoutSpace.match(/^[A-Z]\d+$/i)) {
+            return validationErrorFunc('hoRefNum');
+          }
+        }
+      }
+
+      return super.validateField(key, req);
+    }
+  };
