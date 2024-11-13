18ba1599a71500fdc55a6c594d9b1251acc7e68a
Mike Maratheftis <160880141+MikeMaratheftis@users.noreply.github.com>
2024-11-13 11:33:04 +0000

    RODD-104 Main Applicants Address (#56)
    * ROD-104 main applicant address
    
    ---------
    
    Co-authored-by: Mike Maratheftis <mike.maratheftis@digital.homeoffice.gov.uk>

--- apps/rod/sections/summary-data-sections.js
+++ apps/rod/sections/summary-data-sections.js
@@ -75,6 +75,23 @@ module.exports = {
       field: 'main-applicant-nationality'
     },
     {
+      step: '/enter-main-applicant-address',
+      field: 'main-applicant-address-details',
+      parse: (value, req) => {
+        const applicantAddress = [
+          req.sessionModel.get('main-applicant-address-1'),
+          req.sessionModel.get('main-applicant-town-or-city'),
+          req.sessionModel.get('main-applicant-postcode')
+        ];
+        const addressLine2 = req.sessionModel.get('main-applicant-address-2');
+        if (addressLine2) {
+          applicantAddress.splice(1, 0, addressLine2);
+        }
+        req.sessionModel.set('applicantAddress', applicantAddress.join(', '));
+        return applicantAddress.join(', \n');
+      }
+    },
+    {
       step: '/your-documents',
       field: 'document-type',
       parse: (value, req) => {
