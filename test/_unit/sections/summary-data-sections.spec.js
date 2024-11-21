ee6595175f09a3b026230a7a51121b54bf37a1db
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2024-11-21 12:38:58 +0000

    utils unit tests

--- test/_unit/sections/summary-data-sections.spec.js
+++ test/_unit/sections/summary-data-sections.spec.js
@@ -2,7 +2,7 @@ const sections = require('../../../apps/rod/sections/summary-data-sections.js');
 const pages = require('../../../apps/rod/translations/src/en/pages.json');
 
 describe('Apply Summary Data Sections for ROD', () => {
-  describe.only('Sections and Pages', () => {
+  describe('Sections and Pages', () => {
     it('should have a section header in pages and sections', () => {
       const sectionHeaderKey = Object.keys(sections);
       const pagesSectionsHeaderKeys = Object.keys(pages);
